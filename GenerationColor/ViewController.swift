64e32e2c581d188cb00509bf12249c60830139eb
GinoTarino <inothinged@gmail.com>
2021-05-22 15:11:34 +0300

    Homework is done

--- GenerationColor/ViewController.swift
+++ GenerationColor/ViewController.swift
@@ -22,31 +22,58 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var labelResultColor: UILabel!
     
+    private let сonversionRateInRGBValue: Float = 0.0039215686
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         fieldResultColor.layer.cornerRadius = 10
         
-        var  redSliderValue:CGColor = .init(srgbRed: 224.0, green: 24.0, blue: 34.0, alpha: 1.0)
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        
+        redSlider.value = 0.54
+        greenSlider.value = 0.46
+        blueSlider.value = 0.1
+        
+        
     }
-    
-    
     
     @IBAction func moveRedColor() {
-        redValue.text  = "\(CGFloat(redSlider.value))"
+        redValue.text  = "\(Int(redSlider.value / сonversionRateInRGBValue))"
         
+        fieldResultColor.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0)
+                
+        labelResultColor.text = "RGB: (\(redValue.text ?? "0"), \(greenValue.text ?? "0") \(blueValue.text ?? "0"))"
     }
-        
     
     @IBAction func moveGreenColor() {
-        greenValue.text  = "\(CGFloat(greenSlider.value))"
+        greenValue.text  = "\(Int(greenSlider.value / сonversionRateInRGBValue))"
+        
+        fieldResultColor.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0)
+        
+        labelResultColor.text = "RGB: (\(redValue.text ?? "0"), \(greenValue.text ?? "0") \(blueValue.text ?? "0"))"
     }
     
     @IBAction func moveBlueColor() {
-        blueValue.text  = "\(CGFloat(blueSlider.value))"
+        blueValue.text  = "\(Int(blueSlider.value / сonversionRateInRGBValue))"
+        
+        fieldResultColor.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0)
+        
+        labelResultColor.text = "RGB: (\(redValue.text ?? "0"), \(greenValue.text ?? "0") \(blueValue.text ?? "0"))"
     }
-    
-    @IBAction func tappingForCopyColor() {
-    }
-    
+
 }
